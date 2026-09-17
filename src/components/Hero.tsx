d9fb9b8d8ef89fae2cc401fc201b25d80ce8bb18
Cornelius Toole <waitaminutedigital@outlook.com>
2026-09-17 18:49:46 -0500

    chore: remove phone number CTA from hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -156,23 +156,6 @@ export function Hero() {
                 </svg>
                 DM @nmbgjay to Sign Up
               </a>
-
-              <a href="sms:12056029688" className="cta-btn cta-sms">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
-                </svg>
-                Text (205) 602-9688
-              </a>
             </div>
           </div>
 
